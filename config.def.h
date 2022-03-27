3f29ab5d2f70d688a2d6ab0d6f30dc7a5fdbdc05
Marko Bauhardt <mb@datameer.com>
2022-03-27 19:35:52 +0200

    change font to jetbrains mono

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono,JetBrains Mono:pixelsize=16:style=light";
 static int borderpx = 2;
 
 /*
